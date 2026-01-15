bedafcd78efb2229bd076bb73579383c9544e4d5
ThinkSaurabh <saurabh.kale@medarch.com>
2026-01-16 00:02:39 +0530

    creds.py changes

--- utils/creds.py
+++ utils/creds.py
@@ -3,12 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-if os.getenv("GITHUB_ACTIONS") == "true":
-    url = os.getenv("URL")
-    username = os.getenv("USERNAME")
-    password = os.getenv("PASSWORD")
-else:
-    url = os.getenv("URL")
-    username = os.getenv("User_id")
-    password = os.getenv("Password")
+url = os.getenv('URL')
+username = os.getenv('User_id')
+password = os.getenv('Password')
